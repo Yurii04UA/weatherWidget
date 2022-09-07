edc902862f7dbd4d64b6b1918bdff2b24318452a
Yurii04UA <uyriparaka@gmail.com>
2022-09-07 12:20:12 +0300

    feat: added error handler

--- main.js
+++ main.js
@@ -2,34 +2,38 @@ function WeatherWidgit(element) {
   if (!element) {
     return;
   }
+  let myData = {
+    city: "-",
+    temp: "-",
+    temp_feels_Like: "-",
+    country: "-",
+    description: "-",
+    icon: "-",
+  };
+  function getData(data) {
+    myData = {
+      city: data.name,
+      temp: data.main.temp.toFixed(0),
+      temp_feels_Like: data.main.feels_like.toFixed(0),
+      country: data.sys.country,
+      description: data.weather[0].description,
+      icon: data.weather[0].icon,
+    };
+  }
   async function fetchUrl(url) {
-    let data = {};
     let content;
     let error = "";
     try {
       const response = await fetch(url);
       const result = await response.json();
-      data = {
-        city: result.name,
-        temp: result.main.temp.toFixed(0),
-        temp_feels_Like: result.main.feels_like.toFixed(0),
-        country: result.sys.country,
-        description: result.weather[0].description,
-        icon: result.weather[0].icon,
-      };
-    } catch (e) {
-      error = 'Something went wrong. Try again'
-      data = {
-        city: "-",
-        temp: "-",
-        temp_feels_Like: "-",
-        country: "-",
-        description: "-",
-        icon: "-",
-      };
+      if (result.cod != 200) {
+        throw new Error(result.message);
+      }
+      getData(result);
+    } catch (err) {
+      error = `Something went wrong. Try again.  ${err.message}`;
     }
-    const description =
-      data.description[0].toUpperCase() + data.description.slice(1);
+
     const date = new Date();
     const today = date.toLocaleDateString();
     const time = date.toLocaleTimeString().slice(0, -3);
@@ -39,26 +43,26 @@ function WeatherWidgit(element) {
          <div class="widget">
          
             <div class="widget-left">
-               <div class="city">${data.city}, <span class="country">${data.country}</span></div>
+               <div class="city">${myData.city}, <span class="country">${myData.country}</span></div>
                <div class="date">${today}</div>
                <div class="time">${time}</div>
             </div>
             <div class="widget-right">
                <div class="widget-right_header">
-                  <div class="temp"> ${data.temp} °C</div>
+                  <div class="temp"> ${myData.temp} °C</div>
                   <div class="img">
-                     <img src="http://openweathermap.org/img/wn/${data.icon}@2x.png" alt="">
+                     <img src="http://openweathermap.org/img/wn/${myData.icon}@2x.png" alt="">
                   </div>
                </div>
-               <div class="temp-feels_like">Feels like : ${data.temp_feels_Like} °C</div>
-               <div class="forecast">${description}</div>
+               <div class="temp-feels_like">Feels like : ${myData.temp_feels_Like} °C</div>
+               <div class="forecast"></div>
             </div>
          </div>
        </div>
     `;
 
     let container = document.createElement("div");
-    container.innerHTML =content;
+    container.innerHTML = content;
     element.append(container);
   }
 
@@ -68,7 +72,7 @@ function WeatherWidgit(element) {
 
     fetchUrl(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=3a14ed878c17ea88667c02de9b9be534&units=metric`
-    )
+    );
   });
 }
 
